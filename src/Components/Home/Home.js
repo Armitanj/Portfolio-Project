390c5f4908b08ea3233a99aa79735aa2da750698
Armita Najafabadi <109144538+Armitanj@users.noreply.github.com>
2025-01-29 15:36:15 +0330

    file changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
           <Col xl={5} lg={5} md={5} sm={12} className="text-center">
             <div className="homePic">
               <img
-                src="./images/Armita33.jpg"
+                src="./images/Armita.jpg"
                 alt="my-pic"
                 className="img-fluid rounded"
               />
